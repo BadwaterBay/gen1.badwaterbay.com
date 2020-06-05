d6a993d06e3b82808e03c14396598c554f35db95
tangaw <aaronwntang@gmail.com>
2020-06-05 14:48:18 -0700

    Set up home page

---
+++ public_html/index.js
@@ -0,0 +1,22 @@
+import head from './components/head.js';
+import navbar from './components/navbar.js';
+import footer from './components/footer.js';
+
+const mainContent = () => {
+  return `
+    <div class="container mt-4">
+      <p>You are at badwaterbay.com!</p>
+      <p>Visit us on <a href="https://github.com/BadwaterBay" target="_blank" rel="noreferrer">GitHub</a>.</p>
+      <ul class="list-group">
+        <li class="list-group-item list-group-item-action">
+          <a href="github-label-manager-plus/">GitHub Label Manager Plus</a>
+        </li>
+      </ul>
+    </div>
+  `
+}
+
+document.getElementById('head-anchor').innerHTML += head();
+document.getElementById('navbar-anchor').innerHTML += navbar();
+document.getElementById('content-anchor').innerHTML += mainContent();
+document.getElementById('footer-anchor').innerHTML += footer();
