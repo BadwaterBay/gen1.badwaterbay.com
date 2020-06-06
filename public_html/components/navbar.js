7e2389028d2137c9baf43be41451e70ae55d7f7f
Skyler Dong <dongskyler@gmail.com>
2020-06-05 21:26:32 -0500

    Update navbar & add favicon and google analytics tracking

--- public_html/components/navbar.js
+++ public_html/components/navbar.js
@@ -2,14 +2,34 @@ const navbar = () => {
   return `
     <div class="navbar navbar-expand-md navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="/">
           Badwater&nbsp;Bay
         </a>
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Web&nbsp;Apps
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="github-label-manager-plus">
+                GitHub&nbsp;Label&nbsp;Manager&nbsp;Plus
+              </a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              href="about/"
+            >
+              About
+            </a>
+          </li>
+        </ul>
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
             <a
               class="nav-link"
-              href="https://github.com/BadwaterBay/github-label-manager-plus"
+              href="https://github.com/BadwaterBay/"
               target="_blank"
               rel="noreferrer"
             >
