db6d6a9c9b65ef43b24f2ffeeae96f61dbf23a89
tangaw <aaronwntang@gmail.com>
2020-06-08 13:11:44 -0700

    Rewrite masthead styles with simpler declarations

--- public_html/index.js
+++ public_html/index.js
@@ -4,11 +4,7 @@ import footer from './common/components/footer.js';
 const mainContent = () => {
   return `
     <header>
-      <div class="reframe">  
-        <img src="./images/badwater-basin.jpg" alt="Home page image">
-        <div class="overlay"></div>
-      </div>
-
+      <img src="./images/badwater-basin.jpg" alt="Home page image">
       <div class="header-text">
         <h1>Welcome to Badwater&nbsp;Bay!</h1>
         <p>Visit us on <a href="https://github.com/BadwaterBay" target="_blank" rel="noreferrer">GitHub</a></p>
