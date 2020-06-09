8bbd50b606a5103732087218ad276a97038ff4c9
tangaw <aaronwntang@gmail.com>
2020-06-09 00:27:00 -0700

    Insert footer content and styling

--- public_html/common/components/footer.js
+++ public_html/common/components/footer.js
@@ -1,7 +1,44 @@
 const footer = () => {
   return `
     <div id="footer">
-      
+      <div class="container">
+        <ul class="icons">
+          <li>
+            <a
+            href="https://github.com/BadwaterBay/"
+            target="_blank"
+            rel="noreferrer"
+            >
+              <i class="fab fa-github"></i>
+            </a>
+          </li>
+          <li>
+            <a
+            href="https://twitter.com/BadwaterBay"
+            target="_blank"
+            rel="noreferrer"
+            >
+              <i class="fab fa-twitter"></i>
+            </a>
+          </li>
+        </ul>
+        <div id="footer-email">
+          <a
+          href="mailto:info@badwaterbay.com"
+          target="_blank"
+          rel="noreferrer"
+          >
+            <i class="fas fa-envelope"></i>
+            <span>info@badwaterbay.com</span>
+          </a>
+        </div>
+        <div id="footer-links">
+        </div>
+        <hr>
+        <div id="copyright">
+          Copyright &copy; 2020 Badwater Bay.
+        </div>
+      </div>
     </div>
   `
 }
