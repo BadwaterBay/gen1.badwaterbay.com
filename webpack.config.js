772ea07b5c164ab989aa6634146e43ae599f065c
Skyler Dong <dongskyler@gmail.com>
2020-08-09 17:11:46 -0500

    Intermediate stage of configuring Webpack bundler

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,48 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'main.bundle.js',
+    chunkFilename: '[path][name].bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            // options: {
+            // name: '[path][name].[ext]',
+            // context: path.resolve(__dirname, "src"),
+            // useRelativePaths: true
+            // }
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      Popper: ['popper.js', 'default'],
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    port: 5000,
+  },
 };
