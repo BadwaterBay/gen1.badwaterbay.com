7d8bb4013ece9b97517897dbfd8538077e1a2fb3
Skyler Dong <dongskyler@gmail.com>
2020-08-13 13:54:52 -0500

    Add 'Login with GitHub' function to GitHub OAuth App Labelcopier (#47)

--- src/apps/labelcopier/src/app.js
+++ src/apps/labelcopier/src/app.js
@@ -24,6 +24,7 @@ import {
   listenForRepoOwnerCheckbox,
   listenForRepoOwnerInput,
   autoCheckRepoOwnerCheckbox,
+  listenForLoginWithGithub,
 } from './js/loginFormFunctions';
 import {
   listenForListEntriesOfKind,
@@ -38,7 +39,7 @@ import {
   clickOutsideToCloseModal,
 } from './js/commitChanges';
 
-export default () => {
+const app = () => {
   try {
     /**
      * Instantiate Bootstrap-Material-Design
@@ -51,6 +52,7 @@ export default () => {
     listenForRepoOwnerCheckbox();
     listenForRepoOwnerInput();
     autoCheckRepoOwnerCheckbox();
+    listenForLoginWithGithub();
 
     /**
      * Listen for DOM events to manipulate labels and milestones
@@ -93,3 +95,5 @@ export default () => {
     console.error(err);
   }
 };
+
+export default app;
