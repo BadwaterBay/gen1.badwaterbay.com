f2ca7c162c9336e42c80dbe00c46aff47b0cc0f0
Skyler Dong <dongskyler@gmail.com>
2020-08-14 14:34:40 -0500

    Update labelcopier link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,7 +10,7 @@ navbar.innerHTML = (() =>
       <li class="nav-item">
         <a
           class="nav-link"
-          href="apps/labelcopier"
+          href="labelcopier"
         >
           Labelcopier
         </a>
